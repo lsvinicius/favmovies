af86fc11f753253e6600b944b98e05be610f36c8
vinicius <vinicius.lopes.silva89@gmail.com>
2017-03-30 12:31:48 -0300

    Corrected mistake in query_omdb argument

--- src/omdbapi.py
+++ src/omdbapi.py
@@ -1,6 +1,6 @@
 import requests
 
-def query_omdb(**kwargs):
+def query_omdb(kwargs):
     response = {'Response':'False','Error':'Something went wrong.'}
     try:
         response = requests.get("http://www.omdbapi.com/?", params=kwargs)
