1c68287f4100fe9283aea5c7ef873e6caf642fc1
vinicius <vinicius.lopes.silva89@gmail.com>
2017-03-31 11:34:04 -0300

    Added error handling for 404, 401, 403, 500

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_login import LoginManager
 from src.database import db
 from src.views.site_views import site_views
@@ -18,6 +18,13 @@ login_manager.login_view = 'site_views.login'
 def setup_database():
     db.drop_all()
     db.create_all()
+
+@app.errorhandler(404)
+@app.errorhandler(401)
+@app.errorhandler(403)
+@app.errorhandler(500)
+def http_error_handler(error):
+    return render_template('error.jinja', error=error), error.code
 
 @login_manager.user_loader
 def load_user(id):
