8fd8ca718672cb6a64b330d77be37fd64d65c715
vinicius <vinicius.lopes.silva89@gmail.com>
2017-03-31 18:27:53 -0300

    Refactory to make a better code and also easier testing

--- src/restful_app.py
+++ src/restful_app.py
@@ -1,12 +1,17 @@
 from flask import Flask
-from src.database import db
+from src import database
 from src.api.favmovies import favmovies
 
-app = Flask(__name__)
-app.register_blueprint(favmovies)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db.init_app(app)
+def create_app():
+    app = Flask(__name__)
+    app.register_blueprint(favmovies)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    return app
+
+
+app = create_app()
+database.db.init_app(app)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
