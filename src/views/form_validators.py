eb6b6cc173c52b380be00d23042d31aa310f1509
vinicius <vinicius.lopes.silva89@gmail.com>
2017-04-01 10:55:29 -0300

    I fixed some issues found when I was testing the frontend. In case an invalid name was given, the system failed silently, rather than showing the client what happened

--- src/views/form_validators.py
+++ src/views/form_validators.py
@@ -18,18 +18,24 @@ def valid_register_form(form):
     firstname = form.get('firstname')
     if firstname:
         is_valid = re.search(regex, firstname.strip(), re.UNICODE) and is_valid
+        if not is_valid:
+            abort(400, {'description':'Invalid firstname'})
     else:
         abort(400, {'description':'Invalid firstname'})
 
     lastname = form.get('lastname')
     if lastname:
         is_valid = re.search(regex, lastname.strip(), re.UNICODE) and is_valid
+        if not is_valid:
+            abort(400, {'description':'Invalid lastname'})
     else:
         abort(400, {'description':'Invalid lastname'})
 
     password = form.get('password')
     if password:
         is_valid = len(password) > 0 and is_valid
+        if not is_valid:
+            abort(400, {'description':'Invalid password'})
     else:
         abort(400, {'description': 'Invalid password'})
 
