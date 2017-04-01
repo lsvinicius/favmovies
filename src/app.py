6cca68f17cd6ec45ffa0bffd826e13e3dfeff2ba
vinicius <vinicius.lopes.silva89@gmail.com>
2017-04-01 11:06:59 -0300

    Refactored app.py file

--- src/app.py
+++ src/app.py
@@ -1,23 +1,28 @@
 from flask import Flask, render_template
 from flask_login import LoginManager
-from src.database import db
+from src import database
 from src.views.site_views import site_views
 from src.model.user import User
 import os
 
-app = Flask(__name__)
-app.register_blueprint(site_views)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db.init_app(app)
+def create_app():
+    app = Flask(__name__)
+    app.register_blueprint(site_views)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    return app
+
+app = create_app()
+database.db.init_app(app)
 login_manager = LoginManager()
 login_manager.setup_app(app)
 login_manager.login_view = 'site_views.login'
 
+
 @app.before_first_request
 def setup_database():
-    db.drop_all()
-    db.create_all()
+    database.db.drop_all()
+    database.db.create_all()
 
 @app.errorhandler(404)
 @app.errorhandler(401)
